aea7a1627908e84f180b987b60b7662305a1cd76
BrianOBlanton <Brian_Blanton@Renci.Org>
2015-07-14 13:35:36 -0400

    oops

--- setversion.m
+++ setversion.m
@@ -2,6 +2,7 @@ function setversion
 [r,s]=system('git describe --tags');
 [r,s2]=system('git status | sed -n 1p |awk ''{print $NF}''');
 fid=fopen('ThisVersion','w');
-fprintf(fid,'JHT AdcircViz %s : %s\n',deblank(s),deblank(s2));
+str=sprintf('JHT AdcircViz %s : %s',deblank(s),deblank(s2));
+fprintf(fid,'%s\n',str);
 fclose(fid);
 
