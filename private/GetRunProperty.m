46c054becb150e93346409befccc7432ed7077d3
BrianOBlanton <brian_blanton@renci.org>
2015-07-23 13:59:34 -0400

    changed default response from GetRunProperty

--- private/GetRunProperty.m
+++ private/GetRunProperty.m
@@ -1,10 +1,9 @@
 %%  GetRunProperty
 function retval=GetRunProperty(RP,key)
 
+    retval='unknown';
     idx=find(strcmpi(RP{1},key));
     if ~isempty(idx)
         retval=RP{2}{idx};
-    else
-        retval='Key not found in Run Properties';
     end
 end
