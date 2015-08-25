e1c66d17e7ff2f560b30632264cf736a946b27bd
BrianOBlanton <brian_blanton@renci.org>
2015-08-25 16:46:59 -0400

    updated version number

--- setversion.m
+++ setversion.m
@@ -1,9 +1,11 @@
 function setversion
 [r,s]=system('git describe --tags');
-[r,s2]=system('git status | sed -n 1p |awk ''{print $NF}''');
+%[r,s]=system('git tag  | sed -n 1p');
+%[r,s2]=system('git status | sed -n 1p |awk ''{print $NF}''');
 fid=fopen('ThisVersion','w');
 %str=sprintf('JHT StormSurgeViz-Ncml %s : %s',deblank(s),deblank(s2));
-str=sprintf('JHT StormSurgeViz-Ncml %s',deblank(s));
+str=sprintf('JHT StormSurgeViz-Ncml v:%s',deblank(s));
 fprintf(fid,'%s\n',str);
 fclose(fid);
-
+disp(str)
+%disp(s2)
